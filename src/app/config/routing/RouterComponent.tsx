080a24e4c63107d522a7a43826266e32f0f9144d
olszewm4 <m.olszewski1992@gmail.com>
2018-12-28 21:09:11 +0100

    Update notistack + ErrorBoundary on routing

--- src/app/config/routing/RouterComponent.tsx
+++ src/app/config/routing/RouterComponent.tsx
@@ -1,6 +1,7 @@
 import { PureComponent } from 'react';
 import * as React from 'react';
 import { Route, RouteProps, Switch } from 'react-router-dom';
+import ErrorBoundaryContainer from 'src/app/infrastructure/errorsHandler/ErrorBoundaryContainer';
 import RouteConfiguration from './RouteConfiguration';
 import { RouterProps } from './typings';
 
@@ -8,11 +9,13 @@ class RouterComponent extends PureComponent<RouterProps, any> {
 
     public render() {
         return (
-            <Switch location={this.props.location}>
-                {RouteConfiguration.Routes.map((value: RouteProps, index: number) =>
-                    <Route  {...value} key={index} />
-                )}
-            </Switch>
+            <ErrorBoundaryContainer>
+                <Switch location={this.props.location}>
+                    {RouteConfiguration.Routes.map((value: RouteProps, index: number) =>
+                        <Route  {...value} key={index} />
+                    )}
+                </Switch>
+            </ErrorBoundaryContainer>
         )
     }
 }
